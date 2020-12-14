bfd0c09864a31c0d5da2092d40262c09010ec777
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2020-12-14 12:12:17 -0300

    add / in the next.config.js

--- home/next.config.js
+++ home/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     return [
       {
         source: "/app",
-        destination: `${APP_URL}/app`,
+        destination: `${APP_URL}/app/`,
       },
       {
         source: "/app/:path*",
